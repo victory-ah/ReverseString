f8013cf5160b2e0de9ae3b6ee1c38b412e5a8c8c
Victoria Joseph <victoria.f.joseph@outlook.com>
2019-08-20 13:12:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        public static string ReverseString(string phrase)
+        {
+            string result = ("");
+            for (int i = phrase.Length-1; i >= 0; i--)
+            {
+                result += phrase[i];
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a phrase that you want reversed");
+            Console.WriteLine(ReverseString(Console.ReadLine()));
+        }
+    }
+}
